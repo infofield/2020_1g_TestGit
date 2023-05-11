fb4f90e9cfae45dfdea4c6063196a96de5e67627
Krymov Aleksei <krymovalexey@gmail.com>
2023-05-11 16:48:01 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020_1g_TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
